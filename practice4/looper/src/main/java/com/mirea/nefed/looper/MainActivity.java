40a17cba7e0a58b7f45f637917c3c5b6c9bdacbd
Nick <nikita.n007@mail.ru>
2022-05-24 03:01:05 +0300

    forth done

---
+++ practice4/looper/src/main/java/com/mirea/nefed/looper/MainActivity.java
@@ -0,0 +1,39 @@
+package com.mirea.nefed.looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Message;
+import android.os.SystemClock;
+import android.view.View;
+import android.os.Handler;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    private MyLooper myLooper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myLooper = new MyLooper();
+        myLooper.start();
+    }
+
+    public void onClick(View view) {
+        Message msg = new Message();
+        Bundle bundle = new Bundle();
+        bundle.putString("KEY", "21 Student");
+        msg.setData(bundle);
+        try {
+            TimeUnit.SECONDS.sleep(21);
+            if (myLooper != null) {
+                myLooper.handler.sendMessage(msg);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
